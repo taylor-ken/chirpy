cbe6dc90b51bcf3cff7909db5fd24d305ca83b96
taylor-ken <kenneth.n.taylor@gmail.com>
2025-07-13 22:42:57 -0400

    routing

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -15,15 +14,17 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	fileserverHits := apiConfig{}
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", fileserverHits.middlewareMetricsInc(
-		http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))),
-	))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", fileserverHits.handlerMetrics)
-	mux.HandleFunc("/reset", fileserverHits.handlerReset)
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", fsHandler)
+
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -32,31 +33,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	response := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
-	w.Write([]byte(response))
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	count := cfg.fileserverHits.Load()
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	response := fmt.Sprintf("Hits: %v", count)
-	w.Write([]byte(response))
 }
